7dff6cb970b810c2a497e4d38c10d0a0957c84b9
Ethan Holen <ethanholen@gmail.com>
2021-01-23 22:14:40 -0700

    updated game of life tap

--- Formula/game-of-life.rb
+++ Formula/game-of-life.rb
@@ -4,8 +4,8 @@
 class GameOfLife < Formula
   desc "conways game of life in the terminal"
   homepage "https://github.com/EthanHolen/game-of-life"
-  url "https://github.com/EthanHolen/game-of-life/archive/v1.0.0.tar.gz"
-  sha256 "117899f90989353bf448665c9e8a5a48deaece2b29421bce81a13f044dea93ae"
+  url "https://github.com/EthanHolen/game-of-life/releases/download/1.0.1/game-of-life"
+  sha256 "9ebf8fd4ff14393f049ae10ee2ba013dfae5afddb7ae24214460da1ca40376cf"
   license ""
 
   # depends_on "cmake" => :build
